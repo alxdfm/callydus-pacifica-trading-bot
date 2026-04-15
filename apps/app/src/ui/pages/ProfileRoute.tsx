ea551d009fd7f4091445d52fd4de4f31501fdac2
alxdfm <alexandre_fmachado@hotmail.com>
2026-04-15 08:23:48 -0300

    fix(app): remove SolanaWalletStateBridge wrapper from ProfileRoute

--- apps/app/src/ui/pages/ProfileRoute.tsx
+++ apps/app/src/ui/pages/ProfileRoute.tsx
@@ -1,13 +1,10 @@
 import { SolanaWalletEnvironment } from "../../features/wallet/solana/SolanaWalletEnvironment";
-import { SolanaWalletStateBridge } from "../../features/wallet/solana/SolanaWalletStateBridge";
 import { ProfilePage } from "./ProfilePage";
 
 export function ProfileRoute() {
   return (
     <SolanaWalletEnvironment>
-      <SolanaWalletStateBridge>
-        <ProfilePage />
-      </SolanaWalletStateBridge>
+      <ProfilePage />
     </SolanaWalletEnvironment>
   );
 }
